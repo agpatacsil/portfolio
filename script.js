99acf0d9a3517debfbc50406a0027d531fe1c3c2
Allison Patacsil <allisonpatacsil@allisons-mbp.mynetworksettings.com>
2022-07-05 22:07:21 -0400

    2022 update

---
+++ script.js
@@ -0,0 +1,29 @@
+$(window).scroll(function() {
+  var scroll = $(window).scrollTop();
+  if (scroll > 0) {
+    $("#header").addClass("active");
+  }
+  else{
+    $("#header").removeClass("active");
+  }
+});
+
+
+var faq = document.getElementsByClassName("faq-page");
+var i;
+
+for (i = 0; i < faq.length; i++) {
+    faq[i].addEventListener("click", function () {
+        /* Toggle between adding and removing the "active" class,
+        to highlight the button that controls the panel */
+        this.classList.toggle("active");
+
+        /* Toggle between hiding and showing the active panel */
+        var body = this.nextElementSibling;
+        if (body.style.display === "block") {
+            body.style.display = "none";
+        } else {
+            body.style.display = "block";
+        }
+    });
+}
